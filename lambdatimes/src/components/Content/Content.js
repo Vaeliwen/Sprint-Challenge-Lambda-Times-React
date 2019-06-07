44df251dc6e114f03964ac5e1b7cad88d635ad54
Vaeliwen <zaman@imbriaco.com>
2019-06-07 13:13:18 -0400

    :(

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Tabs from './Tabs';
 import Cards from './Cards';
 
-// Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
 
 export default class Content extends Component {
@@ -18,10 +17,12 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+  this.setState({tabs: tabData, cards: cardData})
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({selected: tab})
   };
 
   filterCards = () => {
@@ -35,8 +36,14 @@ export default class Content extends Component {
       Your algorithim for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
           of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
+        - else, it should only return those cards whose 'tab' matched this.state.selected.  
     */
+   if(this.state.selected = 'all'){
+     this.setState({cards: cardData})
+   }
+   else {
+     this.setState({cards: cardData.tab = this.state.selected})
+   }
     return this.state.cards;
   };
 
@@ -48,7 +55,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab = {this.state.selected} selectTabHandler = {selectTabHandler()} />
         <Cards cards={this.filterCards()} />
       </div>
     );
